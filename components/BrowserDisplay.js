4a1d69e13652c645bc1fc55709934c3cb955902c
bugsounet <bugsounet@bugsounet.fr>
2024-02-24 08:58:48 +0100

    lintified

--- components/BrowserDisplay.js
+++ components/BrowserDisplay.js
@@ -3,62 +3,62 @@ class BrowserDisplay {
     this.browser = {
       url: null,
       running: false
-    }
-    this.timerBrowser = null
-    this.sendNotification = (...args) => { that.sendNotification(...args) }
-    this.translate = (...args) => { return that.translate(...args)}
-    this.config = that.config
-    console.log("[BROWSER] BrowserDisplay Ready")
+    };
+    this.timerBrowser = null;
+    this.sendNotification = (...args) => { that.sendNotification(...args); };
+    this.translate = (...args) => { return that.translate(...args);};
+    this.config = that.config;
+    console.log("[BROWSER] BrowserDisplay Ready");
   }
 
-  preparePopup() {
-    var Browser = document.createElement("webview")
-    Browser.id = "EXT_BROWSER"
-    Browser.scrolling="no"
-    Browser.classList.add("hidden")
-    document.body.appendChild(Browser)
+  preparePopup () {
+    var Browser = document.createElement("webview");
+    Browser.id = "EXT_BROWSER";
+    Browser.scrolling="no";
+    Browser.classList.add("hidden");
+    document.body.appendChild(Browser);
   }
 
-  displayBrowser() {
-    logBrowser("Loading", this.browser.url)
-    var webView = document.getElementById("EXT_BROWSER")
-    webView.src= this.browser.url
-    this.startBrowser()
+  displayBrowser () {
+    logBrowser("Loading", this.browser.url);
+    var webView = document.getElementById("EXT_BROWSER");
+    webView.src= this.browser.url;
+    this.startBrowser();
 
     webView.addEventListener("did-fail-load", () => {
-      console.log("[BROWSER] Loading error")
+      console.log("[BROWSER] Loading error");
       this.sendNotification("EXT_ALERT", {
         message: this.translate("BrowserError"),
         type: "error"
-      })
-      this.endBrowser()
-    })
+      });
+      this.endBrowser();
+    });
     webView.addEventListener("crashed", (event) => {
-      console.log("[BROWSER] J'ai tout pété mon général !!!")
-      console.log("[BROWSER]", event)
+      console.log("[BROWSER] J'ai tout pété mon général !!!");
+      console.log("[BROWSER]", event);
       this.sendNotification("EXT_ALERT", {
         message: this.translate("BrowserCrash"),
         type: "error"
-      })
-      this.endBrowser()
-    })
+      });
+      this.endBrowser();
+    });
     webView.addEventListener("console-message", (event) => {
-      if (event.level == 1 && this.config.debug) console.log("[BROWSER]", event.message)
-    })
+      if (event.level === 1 && this.config.debug) console.log("[BROWSER]", event.message);
+    });
     webView.addEventListener("did-stop-loading", () => {
-      if (this.browser.running || (webView.getURL() == "about:blank")) return
-      this.browser.running = true
-      logBrowser("URL Loaded", webView.getURL())
+      if (this.browser.running || (webView.getURL() === "about:blank")) return;
+      this.browser.running = true;
+      logBrowser("URL Loaded", webView.getURL());
       webView.executeJavaScript(`
       var timer = null
       function scrollDown(posY){
         clearTimeout(timer)
         timer = null
         var scrollHeight = document.body.scrollHeight
-        if (posY == 0) console.log("Begin Scrolling")
+        if (posY === 0) console.log("Begin Scrolling")
         if (posY > scrollHeight) posY = scrollHeight
         document.documentElement.scrollTop = document.body.scrollTop = posY;
-        if (posY == scrollHeight) return console.log("End Scrolling")
+        if (posY === scrollHeight) return console.log("End Scrolling")
         timer = setTimeout(function(){
           if (posY < scrollHeight) {
             posY = posY + ${this.config.scrollStep}
@@ -68,70 +68,70 @@ class BrowserDisplay {
       };
       if (${this.config.scrollActivate}) {
         setTimeout(scrollDown(0), ${this.config.scrollStart});
-      };`)
-    })
+      };`);
+    });
     if (this.config.displayDelay) {
       this.timerBrowser = setTimeout(() => {
-        this.endBrowser(true)
-      }, this.config.displayDelay)
+        this.endBrowser(true);
+      }, this.config.displayDelay);
     }
   }
 
-  startBrowser() {
+  startBrowser () {
     if (!this.browser.running) this.sendNotification("EXT_ALERT", {
       message: this.translate("BrowserOpen"),
       type: "information"
-    })
-    this.sendNotification("EXT_BROWSER-CONNECTED")
-    clearTimeout(this.timerBrowser)
-    this.timerBrowser = null
-    this.hideModules()
-    this.showBrowser()
+    });
+    this.sendNotification("EXT_BROWSER-CONNECTED");
+    clearTimeout(this.timerBrowser);
+    this.timerBrowser = null;
+    this.hideModules();
+    this.showBrowser();
   }
 
-  endBrowser(extAlert=false) {
+  endBrowser (extAlert=false) {
     if (extAlert) this.sendNotification("EXT_ALERT", {
       message: this.translate("BrowserClose"),
       type: "information"
-    })
-    this.sendNotification("EXT_BROWSER-DISCONNECTED")
-    this.hideBrowser()
-    this.resetBrowser()
-    this.showModules()
+    });
+    this.sendNotification("EXT_BROWSER-DISCONNECTED");
+    this.hideBrowser();
+    this.resetBrowser();
+    this.showModules();
   }
 
-  resetBrowser() {
-    clearTimeout(this.timerBrowser)
-    this.timerBrowser = null
+  resetBrowser () {
+    clearTimeout(this.timerBrowser);
+    this.timerBrowser = null;
     this.browser = {
       url: null,
       running: false
-    }
-    var iframe = document.getElementById("EXT_BROWSER")
-    iframe.src= "about:blank"
+    };
+    var iframe = document.getElementById("EXT_BROWSER");
+    iframe.src= "about:blank";
   }
 
-  showBrowser() {
-    logBrowser("Show Iframe")
-    var iframe = document.getElementById("EXT_BROWSER")
-    iframe.classList.remove("hidden")
+  showBrowser () {
+    logBrowser("Show Iframe");
+    var iframe = document.getElementById("EXT_BROWSER");
+    iframe.classList.remove("hidden");
   }
 
-  hideBrowser() {
-    logBrowser("Hide Iframe")
-    var iframe = document.getElementById("EXT_BROWSER")
-    iframe.classList.add("hidden")
+  hideBrowser () {
+    logBrowser("Hide Iframe");
+    var iframe = document.getElementById("EXT_BROWSER");
+    iframe.classList.add("hidden");
   }
 
-  hideModules() {
+  hideModules () {
     MM.getModules().enumerate((module)=> {
-      module.hide(100, {lockString: "EXT_LOCKED"})
-    })
+      module.hide(100, { lockString: "EXT_LOCKED" });
+    });
   }
 
-  showModules() {
+  showModules () {
     MM.getModules().enumerate((module)=> {
-      module.show(100, {lockString: "EXT_LOCKED"})
-    })
+      module.show(100, { lockString: "EXT_LOCKED" });
+    });
   }
 }
--- node_helper.js
+++ node_helper.js
@@ -1,14 +1,15 @@
-"use strict"
+"use strict";
 
-var NodeHelper = require("node_helper")
-var log = (...args) => { /* do nothing */ }
+var NodeHelper = require("node_helper");
+
+var log = (...args) => { /* do nothing */ };
 
 module.exports = NodeHelper.create({
-  socketNotificationReceived: function (noti, payload) {
+  socketNotificationReceived (noti, payload) {
     switch (noti) {
       case "INIT":
-        console.log("[BROWSER] EXT-Browser Version:", require('./package.json').version, "rev:", require('./package.json').rev)
-      break
+        console.log("[BROWSER] EXT-Browser Version:", require("./package.json").version, "rev:", require("./package.json").rev);
+        break;
     }
   }
-})
+});
